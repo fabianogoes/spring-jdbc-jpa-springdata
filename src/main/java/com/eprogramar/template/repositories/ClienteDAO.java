b2df5acde6bd7216e2d23f1157e4991bc67e1189
Fabiano Góes <fabianogoes@gmail.com>
2015-10-23 17:38:43 -0200

    configuração Spring Data

---
+++ src/main/java/com/eprogramar/template/repositories/ClienteDAO.java
@@ -0,0 +1,9 @@
+package com.eprogramar.template.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import com.eprogramar.template.model.Cliente;
+
+@Repository
+public class ClienteDAO extends GenericAbastractDAO<Cliente> {
+}
---
+++ src/main/java/com/eprogramar/template/repositories/PessoaDAO.java
@@ -0,0 +1,9 @@
+package com.eprogramar.template.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import com.eprogramar.template.model.Pessoa;
+
+@Repository
+public class PessoaDAO extends GenericAbastractDAO<Pessoa> {
+}
